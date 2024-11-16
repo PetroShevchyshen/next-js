7f3f3ff1477877d3ddc01fd9ad5cb0e19dbb91b4
Petro <Petro.Shevchyshen@fivesysdev.com>
2024-11-16 20:24:01 +0200

    first changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import styles from "./page.module.css";
-import course from "./course.json";
+import styles from './page.module.css';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <ul>
-        {course.lessons.map((lesson) => {
-          return (
-            <li key={lesson.name}>
-              <h2>{lesson.title}</h2>
-              <p>{lesson.points}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </main>
-  );
+export default async function Home() {
+  return <main className={styles.main}></main>;
 }
